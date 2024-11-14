ba6d0fa4fb564cd163c711cfd0e3cf02072ca858
Joseildo M S filho <joseildo.filho@contaazul.com>
2024-11-13 21:48:45 -0300

    chore: starts experimenting with plenary.job

--- lua/db_connect/init.lua
+++ lua/db_connect/init.lua
@@ -1,28 +1,39 @@
-local pgmoon = require"pgmoon"
+local Job = require 'plenary.job'
 local M = {}
 
 function M.connect()
-    local pg = pgmoon.new({
-        host = "localhost",
-        port = "5460",
-        database = "swp-cash-out-service",
-        user = "postgres",
-        password = "postgres",
+    -- postgresql://[user[:password]@][host][:port][,...][/dbname][?param1=value1&...]
+    local username = 'postgres'
+    local password = 'postgres'
+    local host = 'localhost:5460'
+    local dbname = 'swp-cash-out-service'
+    local url = 'postgresql://' .. username .. ':' .. password .. '@' .. host .. '/' .. dbname
+    print(url)
+    M.connection = Job:new({
+        command = 'psql',
+        args = { url },
+        on_stdout = function(_, data)
+            print("stdout", data)
+        end,
+        on_stderr = function(_, data)
+            print("stderr", data)
+        end,
+        on_exit = function(_, data)
+            print("exit", data)
+        end,
+        interactive = true
     })
-    assert(pg:connect())
+    M.connection:start()
+    return M
 end
 
 vim.api.nvim_create_user_command("DBConnect", function(opts)
-
     print(opts)
     M.connect()
-
 end, {})
 
 function M.setup(opts)
     print(opts)
 end
-
-print("olá mundo")
 
 return M
