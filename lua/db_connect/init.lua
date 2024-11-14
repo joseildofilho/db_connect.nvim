86690cc0128cd4b3a3a6c63513c6d07ede0cbdc4
Joseildo M S filho <joseildo.filho@contaazul.com>
2024-11-14 19:04:45 -0300

    chore: keeps with the implementation for db split

--- lua/db_connect/init.lua
+++ lua/db_connect/init.lua
@@ -1,14 +1,37 @@
+local fidget = require 'fidget'
 local Job = require 'plenary.job'
 local M = {}
 
-function M:connect(opts)
+function M.connect(urlObj)
+    urlObj = urlObj or {}
+    urlObj.url = urlObj.url or "postgresql://postgres:postgres@localhost:5460/swp-cash-out-service"
+    urlObj.name = urlObj.name or "default"
     -- postgresql://[user[:password]@][host][:port][,...][/dbname][?param1=value1&...]
-    local url = 'postgresql://' .. opts.username .. ':' .. opts.password .. '@' .. opts.host .. '/' .. opts.dbname
-    self._connection = Job:new({
+    M._connection = Job:new({
         command = 'psql',
-        args = { url },
+        args = { urlObj.url },
         on_stdout = function(_, data)
-            print("stdout", data)
+            vim.schedule(function()
+                local results_buf = nil
+                for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+                    local buf_name = vim.api.nvim_buf_get_name(buf)
+                    print(buf_name)
+                    if buf_name == 'db_connect://results' then
+                        results_buf = buf
+                        break
+                    end
+                end
+                if results_buf == nil then
+                    local current_buf = vim.api.nvim_get_current_buf()
+
+                    vim.cmd [[vsp db_connect://results]]
+                    results_buf = vim.api.nvim_get_current_buf()
+
+                    vim.api.nvim_set_current_buf(current_buf)
+                end
+
+                vim.api.nvim_buf_set_lines(results_buf, 0, -1, false, data)
+            end)
         end,
         on_stderr = function(_, data)
             print("stderr", data)
@@ -18,19 +41,26 @@ function M:connect(opts)
         end,
         interactive = true
     })
-    self._connection:start()
+    fidget.notify("Connecting to " .. urlObj.name, vim.log.levels.INFO)
+    M._connection:start()
+    fidget.notify("Connected to " .. urlObj.name, vim.log.levels.INFO)
     return M
 end
 
-function M:submitQuery(query)
-    self._connection:send(query)
+function M.submitQuery(query)
+    M._connection:send(query .. '\n')
 end
 
-vim.api.nvim_create_user_command("DbConnectSubmitQuery", function(opts) M:submitQuery(opts) end, {})
-
+vim.api.nvim_create_user_command("DbConnectSubmitQuery", function(opts) M.submitQuery(opts) end, {})
 
 function M.setup(opts)
-    M:connect(opts)
+    if opts.connections then
+        if table.getn(opts.connections) ~= 1 then
+            print("Only one connection is supported")
+            return
+        end
+        M.connect(opts.connections[1]())
+    end
 end
 
 return M
