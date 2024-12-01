631fbf72171029c12bbb68d4312b85546fd4f036
Joseildo Filho <filhojoseildo@gmail.com>
2024-12-01 17:30:38 -0300

    chore: adds a very simple unit test

---
+++ lua/spec/db_connect/queries_cache_spec.lua
@@ -0,0 +1,8 @@
+describe('tests the cache for queries', function()
+    it('should add 1 query only when we do it manually', function()
+        local cache = require 'db_connect.queries_cache'
+        cache.add_query('select * from anything;')
+
+        assert.equals(#cache.query_queue, 1)
+    end)
+end)
