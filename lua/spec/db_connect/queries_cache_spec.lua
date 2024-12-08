d4d2222beb874f69963343abe317f2b898f1f6ea
Joseildo Filho <filhojoseildo@gmail.com>
2024-12-08 19:00:22 -0300

    chore: simple tests and implementatio for a little queue

--- lua/spec/db_connect/queries_cache_spec.lua
+++ lua/spec/db_connect/queries_cache_spec.lua
@@ -1,15 +1,23 @@
 describe('tests the cache for queries', function()
     it('should add 1 query only when we do it manually', function()
-        local cache = require 'db_connect.queries_cache'
-        cache.add_query('select * from anything;')
+        local cache = require 'db_connect.queries_cache':new()
+        local choosed_query = 'select * from anything;'
+        cache:add_query(choosed_query)
 
-        assert.equals(#cache.query_queue, 1)
+        assert.equals(#cache:get_cached_queries(), 1)
+        assert.equals(cache:get_cached_queries()[1], choosed_query)
     end)
 
-    it('should add 1 query only when we do it manually', function()
-        local cache = require 'db_connect.queries_cache'
-        cache.add_query('select * from anything;')
+    it('should add 2 queries, but the cache limit is 1', function()
+        local cache = require 'db_connect.queries_cache':new({
+            cache_limit = 1
+        })
 
-        assert.equals(#cache.query_queue, 1)
+        local second_query = 'select * from somewhere;'
+        cache:add_query('select * from anything;')
+        cache:add_query(second_query)
+
+        assert.equals(cache:get_cached_queries()[2], second_query)
+        assert.equals(cache:get_cached_queries()[1], nil)
     end)
 end)
